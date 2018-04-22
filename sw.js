d51e42d286073b29ab3a2f1fd577c5e174d3dd5c
Glitch (service-workerino) <none>
2018-04-22 16:27:17 +0000

    🍍🎺 Checkpoint ./sw.js:346598/520

--- sw.js
+++ sw.js
@@ -19,6 +19,16 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    
+    caches.match(event.request)
+      .then(function(response) {
+        // IF Cache hit, return response
+        if (response) {
+          return response;
+        }
+        
+        var fetchRequest = event.request.clone();
+                      
+        return fetch(fetchRequest).then(f));
+      })
   );
 });
